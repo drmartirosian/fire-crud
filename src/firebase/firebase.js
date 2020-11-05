9ca270595dc37be918c45a7a78a37482368039b7
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-11-05 14:24:14 -0600

    1st commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+  import firebase from 'firebase';
+  
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyB-oFkNu4PRAEGIl8XEVhKUlsLf6xR0JYU",
+    authDomain: "moms-art-site.firebaseapp.com",
+    databaseURL: "https://moms-art-site.firebaseio.com",
+    projectId: "moms-art-site",
+    storageBucket: "moms-art-site.appspot.com",
+    messagingSenderId: "827169318284",
+    appId: "1:827169318284:web:e06c9170766717c30ba2a9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
